b1eeef4f25df974bd4295be694d18af9d9f9694e
JateX <grisaia@inbox.ru>
2023-05-27 07:53:29 +0300

    Task_2 complete

---
+++ Task_2_RD/src/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <set>
+
+int main(void) {
+	std::set<int> rd;
+	unsigned count = 0;
+
+	std::cout << "[IN]:" << std::endl;
+	std::cin >> count;
+	while (count != 0) {
+		int var = 0;
+		std::cin >> var;
+		rd.insert(var);
+		count--;
+	}
+
+	std::cout << "[OUT]:" << std::endl;
+	std::set<int>::reverse_iterator rit;
+	for (rit = rd.rbegin(); rit != rd.rend(); rit++)
+		std::cout << *rit << std::endl;
+	return 0;
+}

441cff9e057c4bd09ce456506dc633c1b59a440e
JateX <grisaia@inbox.ru>
2023-05-28 09:19:26 +0300

    Task_3 complete

--- Task_3_AV/src/Vector/Vector.h
+++ Task_3_AV/src/Vector/Vector.h
@@ -1,20 +1,23 @@
 ﻿#pragma once
-#include <iostream>
 #include <iterator>
 
 template<class T>
-class OwnIterator;
+class Iterator;
 
 template<class T>
 class Vector {
 public:
-	typedef OwnIterator<T> iterator;
-	typedef OwnIterator<const T> const_iterator;
+	typedef Iterator<T> iterator;
+	typedef Iterator<const T> const_iterator;
 
 	Vector(std::initializer_list<T> values) : _size(values.size()), _capacity(_size), p_data(new T[_capacity]) {
 		std::copy(values.begin(), values.end(), p_data.get());
 	}
 
+	// Версия инициализации без параметров
+	Vector() : _size(0), _capacity(0), p_data(new T[_capacity]) {}
+
+	// получение элемента с обработкой ошибок
 	T at(unsigned int index) {
 		try {
 			return return_value(index);
@@ -24,23 +27,29 @@ public:
 		}
 	}
 
-	void push_back(T value) {
+	// Добавить элемент в контейнер 
+	void push_back(const T value) {
 		if (this->capacity() > this->size()) {
-
+			this->_size++;
+			this->p_data[this->_size - 1] = value;
 		}
 		else if (this->capacity() <= this->size()) {
-
+			increase_capacity();
+			this->_size++;
+			this->p_data[this->_size -1] = value;
 		}
 	}
 
+	// Получить размер контейнера с элементами
 	const unsigned int size() const {
 		return _size;
 	}
 
+	// Получить фактический размер выделенной памяти под контейнер
 	const unsigned int capacity() const {
 		return _capacity;
 	}
-	
+
 	// Iterators
 	iterator begin() {
 		return iterator(p_data.get());
@@ -50,19 +59,34 @@ public:
 		return iterator(p_data.get() + _size);
 	}
 
-	const_iterator begin() const {
+	const const_iterator begin() const {
 		return const_iterator(p_data.get());
 	}
 
-	const_iterator end() const {
+	const const_iterator end() const {
 		return const_iterator(p_data.get() + _size);
 	}
 
 private:
+	// Functions
 	const T return_value(unsigned int& _index) const {
 		if (_index >= this->size())
 			throw std::out_of_range("[ERROR]: Index greter than size!");
 		return p_data[_index];
+	}
+
+	void increase_capacity() {
+		this->_capacity += this->_size + 1;
+		std::unique_ptr<T[]> p_temp(new T[_capacity]);
+		for (unsigned int i = 0; i < _size; i++)
+			p_temp[i] = this->p_data[i];
+
+		this->p_data.reset();
+		this->p_data = std::make_unique<T[]>(_capacity);
+		for (unsigned int i = 0; i < _size; i++)
+			this->p_data[i] = p_temp[i];
+
+		p_temp.reset();
 	}
 
 	// Variables
@@ -71,32 +95,32 @@ private:
 	std::unique_ptr<T[]> p_data;
 };
 
-
+// Для range based for
 template<class T>
-class OwnIterator : public std::iterator<std::input_iterator_tag, T> {
+class Iterator : public std::iterator<std::input_iterator_tag, T> {
 	friend class Vector<T>;
 public:
-	OwnIterator(const OwnIterator& it) : p_it(it.p_it) {}
+	Iterator(const Iterator& it) : p_it(it.p_it) {}
 
-	bool operator!=(OwnIterator const& other) const {
+	bool operator!=(Iterator const& other) const {
 		return p_it != other.p_it;
 	}
 
-	bool operator==(OwnIterator const& other) const {
+	bool operator==(Iterator const& other) const {
 		return p_it == other.p_it;
 	}
 
-	typename OwnIterator::reference operator*() const {
+	typename Iterator::reference operator*() const {
 		return *p_it;
 	}
 
-	OwnIterator& operator++() {
+	Iterator& operator++() {
 		++p_it;
 		return *this;
 	}
 
 private:
-	OwnIterator(T* p) : p_it(p) {}
+	Iterator(T* p) : p_it(p) {}
 
 	T* p_it = nullptr;
 };
--- Task_3_AV/src/main.cpp
+++ Task_3_AV/src/main.cpp
@@ -2,10 +2,88 @@
 #include "Vector/Vector.h"
 
 int main(void) {
-	Vector<int> vec = {1, 2};
-	for (auto& i : vec) {
-		i = 1;
-		std::cout << i << std::endl;
-	}
+	// int
+	std::cout << "Vector type INT:" << std::endl;
+	Vector<int> int_vec = { 1, 2 }; // Поддерживает список инициализации
+	for (const auto& i : int_vec)
+		std::cout << i << " ";
+	std::cout << "\t\t[size]:" << int_vec.size() << " [capacity]: " << int_vec.capacity() << std::endl;
+	
+	int_vec.push_back(3);
+	for (const auto& i : int_vec)
+		std::cout << i << " ";
+	std::cout << "\t\t[size]:" << int_vec.size() << " [capacity]: " << int_vec.capacity() << std::endl;
+
+	int_vec.push_back(3);
+	for (const auto& i : int_vec)
+		std::cout << i << " ";
+	std::cout << "\t[size]:" << int_vec.size() << " [capacity]: " << int_vec.capacity() << std::endl;
+
+	int_vec.push_back(43);
+	int_vec.push_back(12);
+	for (const auto& i : int_vec)
+		std::cout << i << " ";
+	std::cout << "\t[size]:" << int_vec.size() << " [capacity]: " << int_vec.capacity() << std::endl;
+
+	// empty vector
+	std::cout << "\nEmpty vector type INT:" << std::endl;
+	Vector<int> empty_vec; 
+	for (const auto& i : empty_vec)
+		std::cout << i << " ";
+	std::cout << "\t\t[size]:" << empty_vec.size() << " [capacity]: " << empty_vec.capacity() << std::endl;
+
+	empty_vec.push_back(1);
+	for (const auto& i : empty_vec)
+		std::cout << i << " ";
+	std::cout << "\t\t[size]:" << empty_vec.size() << " [capacity]: " << empty_vec.capacity() << std::endl;
+
+	empty_vec.push_back(12);
+	empty_vec.push_back(15);
+	for (const auto& i : empty_vec)
+		std::cout << i << " ";
+	std::cout << "\t[size]:" << empty_vec.size() << " [capacity]: " << empty_vec.capacity() << std::endl;
+
+	empty_vec.push_back(12);
+	for (const auto& i : empty_vec)
+		std::cout << i << " ";
+	std::cout << "\t[size]:" << empty_vec.size() << " [capacity]: " << empty_vec.capacity() << std::endl;
+
+	// Vector type char
+	std::cout << "\nVector type CHAR:" << std::endl;
+	Vector<char> char_vec;
+	for (const auto& i : char_vec)
+		std::cout << i << " ";
+	std::cout << "\t\t[size]:" << char_vec.size() << " [capacity]: " << char_vec.capacity() << std::endl;
+	
+	char_vec.push_back('a');
+	char_vec.push_back('b');
+	for (const auto& i : char_vec)
+		std::cout << i << " ";
+	std::cout << "\t\t[size]:" << char_vec.size() << " [capacity]: " << char_vec.capacity() << std::endl;
+	
+	char_vec.push_back('c');
+	char_vec.push_back('d');
+	char_vec.push_back('e');
+	for (const auto& i : char_vec)
+		std::cout << i << " ";
+	std::cout << "\t[size]:" << char_vec.size() << " [capacity]: " << char_vec.capacity() << std::endl;
+
+	// Vector type std::string
+	std::cout << "\nVector type STD::STRING:" << std::endl;
+	Vector<std::string> string_vec = { "Hello" };
+	for (const auto& i : string_vec)
+		std::cout << i << " ";
+	std::cout << "\t\t[size]:" << string_vec.size() << " [capacity]: " << string_vec.capacity() << std::endl;
+
+	string_vec.push_back("world");
+	for (const auto& i : string_vec)
+		std::cout << i << " ";
+	std::cout << "\t[size]:" << string_vec.size() << " [capacity]: " << string_vec.capacity() << std::endl;
+
+	string_vec.push_back("!");
+	string_vec.push_back("!");
+	for (const auto& i : string_vec)
+		std::cout << i << " ";
+	std::cout << "[size]:" << string_vec.size() << " [capacity]: " << string_vec.capacity() << std::endl;
 	return 0;
 }
